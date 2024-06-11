409d760c1f27f99a168e4d89a5b6996f64eb40c1
Dennis Cao <dennisrcao@gmail.com>
2024-06-10 21:42:24 -0700

    Fix white spacing issue

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,12 @@
+// frontend/app/layout.tsx
+
 "use client";
+import '../styles/globals.scss';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { SessionProvider } from "next-auth/react"; //used to wrap the useSession in AuthButton.tsx
+import { SessionProvider } from "next-auth/react"; // Used to wrap the useSession in AuthButton.tsx
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export default function RootLayout({
   children,
